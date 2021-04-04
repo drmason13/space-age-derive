2b7ed2cdd942e4a60db0615aa38ca69c918d45b5
drmason13 <drmason13@users.noreply.github.com>
2021-04-04 22:30:44 +0100

    feat: improved error reporting using syn::Error instead of panic
    Also rustfmt to tidy up

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
+//use core::compile_error;
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, LitFloat, DeriveInput};
+use syn::{parse_macro_input, DeriveInput, LitFloat};
 
 #[proc_macro_derive(Planet, attributes(orbital_period))]
 pub fn derive_planet(input: TokenStream) -> TokenStream {
@@ -9,22 +10,36 @@ pub fn derive_planet(input: TokenStream) -> TokenStream {
     let name = &input.ident;
 
     // Find the orbital_period helper attribute! (it is required!)
-    let attr = input.attrs.iter().find(|attr| attr.path.is_ident("orbital_period")).unwrap();
+    let attr = input
+        .attrs
+        .iter()
+        .find(|attr| attr.path.is_ident("orbital_period"))
+        .unwrap();
 
-    // Parse the argument to the attribute as a float
-    // let orbital_period: LitFloat = attr.parse_args().unwrap();
+    // Parse the value of the attribute as a float
     let orbital_period: LitFloat = match attr.parse_meta().unwrap() {
         syn::Meta::NameValue(name_value) => {
             if let syn::Lit::Float(x) = name_value.lit {
                 x
             } else {
-                panic!("expected a float value, e.g. #[orbital_period = 1.0]")
+                return syn::Error::new_spanned(
+                    &name_value.lit,
+                    "expected a float value, e.g. #[orbital_period = 1.0]",
+                )
+                .to_compile_error()
+                .into();
             }
-        },
-        _ => panic!("expected a name = value style syntax, e.g. #[orbital_period = 1.0]")
+        }
+        anything_else => {
+            return syn::Error::new_spanned(
+                &anything_else,
+                "expected a name = value style syntax, e.g. #[orbital_period = 1.0]",
+            )
+            .to_compile_error()
+            .into()
+        }
     };
-    
-    // The generated impl for Planet using the orbital_period from the derive helper attribute
+
     let expanded = quote! {
         impl space_age_core::Planet for #name {
             const ORBITAL_PERIOD: f64 = #orbital_period;
