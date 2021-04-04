8d7ac17ae99507a58f24e3eb20e7531e58db80b7
drmason13 <drmason13@users.noreply.github.com>
2021-04-04 16:49:25 +0100

    fix: replace attribute macro with a derive that uses a helper attribute

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,26 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, LitFloat, ItemStruct};
+use syn::{parse_macro_input, LitFloat, ItemStruct, DeriveInput};
 
-#[proc_macro_attribute]
-pub fn orbital_period(args: TokenStream, input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as ItemStruct);
+#[proc_macro_derive(Planet, attributes(orbital_period))]
+pub fn derive_planet(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
     let name = &input.ident;
 
-    // Parse the argument to the attribute as a float
-    let orbital_period = parse_macro_input!(args as LitFloat);
+    // Find the orbital_period helper attribute! (it is required!)
+    let attr = input.attrs.iter().filter(|attr| attr.path.is_ident("orbital_period")).next().unwrap();
 
-    // expand to the struct and an imnpl for Planet using the orbital_period from the attribute
+    // Parse the argument to the attribute as a float
+    let orbital_period: LitFloat = attr.parse_args().unwrap();
+    
+    // The generated impl for Planet using the orbital_period from the derive helper attribute
     let expanded = quote! {
-        #input
-        
         impl crate::Planet for #name {
             const ORBITAL_PERIOD: f64 = #orbital_period;
         }
     };
 
-    // Hand the resulting struct and trait impl back to the compiler
+    // Return the generated trait impl back to the compiler
     TokenStream::from(expanded)
 }
